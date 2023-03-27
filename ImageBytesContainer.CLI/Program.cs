9689076c48477a1580bfd3240cec6705ef854aba
Sameer Niaz <sameermohniaz@gmail.com>
2023-03-27 13:30:46 -0400

    Initial commit

---
+++ ImageBytesContainer.CLI/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace ImageBytesContainer.CLI;
+
+class Program
+{
+    private static void Encode(string text)
+    {
+        IBCEncoder.EncodeToImage(text, "output.png");
+        Console.WriteLine("Encoding complete.");
+    }
+
+    private static void Decode(string filePath)
+    {
+        var decodedString = IBCEncoder.DecodeFromImage(filePath);
+        Console.WriteLine();
+        Console.WriteLine("Decoded text:");
+        Console.Write(decodedString);
+        Console.WriteLine();
+        Console.WriteLine("Decoding complete.");
+    }
+
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("ImageBytesContainer by Sameer Niaz");
+
+        if (args.Length < 1)
+        {
+            Console.WriteLine("Running in prompt mode.");
+            Console.WriteLine("Press E to encode. Press D to decode. Press any other key to exit.");
+            var inputKey = Console.ReadKey();
+            Console.WriteLine();
+            
+            if (inputKey.Key == ConsoleKey.E)
+            {
+                Console.WriteLine("The encoded image will be outputted to a file named 'output.png' next to the executable.");
+                Console.WriteLine("Enter text to be encoded:");
+                var inputString = Console.ReadLine();
+                Encode(inputString);
+            }
+            else if (inputKey.Key == ConsoleKey.D)
+            {
+                Console.WriteLine("The decoded text will be outputted to the console.");
+                Console.WriteLine("Enter file path of the image to be decoded:");
+                var filePath = Console.ReadLine();
+                Decode(filePath);
+            }
+            else
+            {
+                return;
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Program execution complete. Press any key to exit.");
+            Console.ReadKey();
+
+            return;
+        }
+        
+        Console.WriteLine("Support for program arguments has not been implemented. Press any key to exit.");
+        Console.ReadKey();
+    }
+}
---
+++ ImageBytesContainer/IBCEncoder.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using SkiaSharp;
+
+namespace ImageBytesContainer;
+
+// ReSharper disable once InconsistentNaming
+public static class IBCEncoder
+{
+    public static void EncodeToImage(string text, string fileDestinationPath)
+    {
+        var bytes = Encoding.UTF8.GetBytes(text);
+        var size = CalculateSize(bytes.Length);
+        var bitmap = new SKBitmap(size.width, size.height);
+
+        if (!bitmap.ReadyToDraw)
+            throw new ApplicationException("Cannot draw on bitmap.");
+
+        for (var byteIndex = 0; byteIndex < bytes.Length; byteIndex++)
+        {
+            var color = new SKColor(bytes[byteIndex], 0, 0);
+            var x = byteIndex % size.width;
+            var y = byteIndex / size.width;
+            bitmap.SetPixel(x, y, color);
+        }
+
+        using var fileStream = File.OpenWrite(fileDestinationPath);
+        var result = bitmap.Encode(fileStream, SKEncodedImageFormat.Png, 100);
+
+        if (!result)
+            throw new ApplicationException("Failed to encode to image file.");
+    }
+
+    public static string DecodeFromImage(string fileSourcePath)
+    {
+        using var fileStream = File.OpenRead(fileSourcePath);
+        var bitmap = SKBitmap.Decode(fileStream);
+        
+        if (bitmap == null)
+            throw new ApplicationException("Failed to decode to image file.");
+        
+        var bytes = new byte[bitmap.Pixels.Length];
+
+        for (var pixelIndex = 0; pixelIndex < bytes.Length; pixelIndex++)
+        {
+            var color = bitmap.GetPixel(pixelIndex, 0);
+            if (color.Alpha == 0) // Reached end of bytes
+                break;
+            bytes[pixelIndex] = color.Red;
+        }
+
+        var outputString = Encoding.UTF8.GetString(bytes);
+        return outputString;
+    }
+
+    private static (int width, int height) CalculateSize(int numberOfBytes)
+    {
+        // Find square side length
+        var width = (int)Math.Ceiling(Math.Sqrt(numberOfBytes));
+        return (width, width);
+    }
+}
